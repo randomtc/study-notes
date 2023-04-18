a44a686301c8c6143bfce4328dd1f1bdcda9f294
Ge Shihao <2714780821@qq.com>
2023-04-18 15:21:42 +0800

    updata

--- code/useGetData.ts
+++ code/useGetData.ts
@@ -1,57 +1,64 @@
 /**
-* 函数参数
-* @param  networkRequest   网络请求
-* @param  addParams        添加参数
-* 返回参数
-* @returnParams
-* @param  parame           请求参数
-* @param  setParame        设置参数
-* @param  data             请求成功数据
-* @param  trigger          重新请求的开关
-* @param  loading          请求状态
-*/
-import { useEffect, useState } from 'react'
+ * 函数参数
+ * @param  networkRequest   网络请求
+ * @param  addParams        添加参数
+ * 返回参数
+ * @returnParams
+ * @param  parame           请求参数
+ * @param  setParame        设置参数
+ * @param  data             请求成功数据
+ * @param  trigger          重新请求的开关
+ * @param  loading          请求状态
+ */
+import { useEffect, useState } from "react"
 
 type ResData<T> = {
-    data: T[]
-    total?: number
-    [k: string]: any
+  data: T[]
+  total?: number
+  [k: string]: any
 }
 
 type Res<T> = {
-    code: number
-    data: ResData<T>
-    message?: string
-    [k: string]: any
+  code: number
+  data: ResData<T>
+  message?: string
+  [k: string]: any
 }
 
-const useGetData = <T>(networkRequest: any, addParams?: Record<string, any>) => {
-    const [data, setData] = useState<ResData<T>>()
-    const [params, setParams] = useState<Record<string, any>>({ page: 1, page_size: 10, ...addParams })
-    const [trigger, setTrigger] = useState<boolean>(false)
-    const [loading, setLoading] = useState<boolean>(false)
+const useGetData = <T>(
+  networkRequest: any,
+  addParams?: Record<string, any>
+) => {
+  const [data, setData] = useState<ResData<T>>()
+  const [params, setParams] = useState<Record<string, any>>({
+    page: 1,
+    page_size: 10,
+    ...addParams,
+  })
+  const [trigger, setTrigger] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
 
-    useEffect(() => {
-        sendRequest()
-    }, [params, trigger])
+  useEffect(() => {
+    params?.isSend && sendRequest()
+  }, [params, trigger])
 
-    async function sendRequest() {
-        setLoading(true)
-        try {
-            const res: Res<T> = await networkRequest(params)
-            if (res?.code === 200) {
-                setData(res?.data)
-            } else {
-                console.error(res)
-            }
-        } catch (err) {
-            console.error(err)
-        } finally {
-            setLoading(false)
-        }
+  async function sendRequest() {
+    setLoading(true)
+    try {
+      const res: Res<T> = await networkRequest(params)
+      if (res?.code === 200) {
+        setData(res?.data)
+      } else {
+        console.error(res)
+      }
+    } catch (err) {
+      console.error(err)
+    } finally {
+      setLoading(false)
     }
+  }
 
-    return { params, setParams, data, trigger, setTrigger, loading }
+  return { params, setParams, data, trigger, setTrigger, loading }
 }
 
 export default useGetData
