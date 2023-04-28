23c802ed37dfee58bc5dfdcb0576fa1c936722d1
Ge Shihao <2714780821@qq.com>
2023-04-28 10:18:40 +0800

    updata

--- code/index.d.ts
+++ code/index.d.ts
@@ -1,2 +1,2 @@
 declare type E<T extends HTMLElement> = React.SyntheticEvent<T> //e参数类型
-declare type Item<T> = Record<string | number | symbol, any> | T
+declare type Item<T> = Record<string | number | symbol, any> | T //遍历项类型
