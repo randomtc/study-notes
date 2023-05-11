0a7e18aeba969d064b61bd8b1cb17b1ff33a30e3
Ge Shihao <2714780821@qq.com>
2023-05-11 10:04:09 +0800

    updata

--- code/useSetColSpan.ts
+++ code/useSetColSpan.ts
@@ -2,23 +2,35 @@ import React from "react"
 import { useGetWindowsSize } from "./useGetWindowsSize"
 const useSetColSpan = () => {
   const bodyWidth = useGetWindowsSize()?.bodyWidth
-  const [span, setSpan] = React.useState<number>()
-  const calculateSpan = React.useCallback((width: number) => {
-    if (width < 840) {
+  return React.useMemo(() => {
+    if (bodyWidth < 840) {
       return 20
-    } else if (width < 1400) {
+    } else if (bodyWidth < 1400) {
       return 12
-    } else if (width < 1700) {
+    } else if (bodyWidth < 1700) {
       return 8
     } else {
       return 6
     }
-  }, [])
-  React.useEffect(() => {
-    setSpan(calculateSpan(bodyWidth as number))
-  }, [bodyWidth, calculateSpan])
-  const returnSpan = React.useMemo(() => span, [span])
-  return returnSpan
+  }, [bodyWidth])
+  //   const bodyWidth = useGetWindowsSize()?.bodyWidth
+  //   const [span, setSpan] = React.useState<number>()
+  //   const calculateSpan = React.useCallback((width: number) => {
+  //     if (width < 840) {
+  //       return 20
+  //     } else if (width < 1400) {
+  //       return 12
+  //     } else if (width < 1700) {
+  //       return 8
+  //     } else {
+  //       return 6
+  //     }
+  //   }, [])
+  //   React.useEffect(() => {
+  //     setSpan(calculateSpan(bodyWidth as number))
+  //   }, [bodyWidth, calculateSpan])
+  //   const returnSpan = React.useMemo(() => span, [span])
+  //   return returnSpan
 }
 
 export default useSetColSpan
