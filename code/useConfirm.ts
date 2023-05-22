844e98c45909b17eedd8f96abb28cf1060482ba5
Ge Shihao <2714780821@qq.com>
2023-05-22 18:10:41 +0800

    updata

--- code/useConfirm.ts
+++ code/useConfirm.ts
@@ -1,59 +1,63 @@
 /**
- * @param  request     网络请求
- * @param  params      请求参数
- * @param  success     成功后的操作
- * @param  tip         提示信息
- * @param  loading     请求状态
+ * @param  request       网络请求
+ * @param  params        请求参数
+ * @param  success       成功后的操作
+ * @param  tip           提示信息
+ * @param  submitLoading 请求状态
+ * @param  submitId      提交Id
  */
 import React from "react"
 import { message } from "antd"
 
-interface ConfirmType {
+interface SubmitType {
   request?: (params?: Record<string, any>) => Promise<any> | void
   params: Record<string, any>
-  success?: (...set:any) => void
+  success?: (...set: any) => void
   tip?: string | null
 }
-
-interface UseConfirmReturn {
-  setConfirm: React.Dispatch<React.SetStateAction<ConfirmType>>
-  loading: boolean
+type Id = string | number | undefined
+interface UseSubmitReturn {
+  setSubmit: React.Dispatch<React.SetStateAction<SubmitType>>
+  submitLoading: boolean
+  submitId?: Id
+  setSubmitId?: React.Dispatch<React.SetStateAction<Id>>
 }
 
-const useConfirm = (): UseConfirmReturn => {
-  const [confirm, setConfirm] = React.useState<ConfirmType>({
+const useSubmit = (): UseSubmitReturn => {
+  const [submit, setSubmit] = React.useState<SubmitType>({
     request: () => {},
     params: {},
     success: () => {},
-    tip: null,
+    tip: null
   })
-  const [loading, setLoading] = React.useState<boolean>(false)
+  const [submitLoading, setSubmitLoading] = React.useState<boolean>(false)
+  const [submitId, setSubmitId] = React.useState<Id>()
 
   const sendRequest = React.useCallback(async () => {
-    setLoading(true)
+    setSubmitLoading(true)
     try {
-      const res = await confirm?.request?.(confirm?.params)
+      const res = await submit?.request?.(submit?.params)
       if (res?.code === 200) {
-        confirm?.success!(res)
-        message.success(confirm?.tip ?? "操作成功")
+        submit?.success!(res)
+        message.success(submit?.tip ?? "操作成功")
       } else {
         console.error(res)
       }
     } catch (err) {
       console.error(err)
     } finally {
-      setConfirm({ params: {} })
-      setLoading(false)
+      setSubmit({ params: {} })
+      setSubmitLoading(false)
     }
-  }, [confirm])
+  }, [submit])
 
   React.useEffect(() => {
-    if (Object.keys(confirm?.params).length) {
+    if (Object.keys(submit?.params).length) {
       sendRequest()
     }
-  }, [confirm, sendRequest])
+  }, [submit, sendRequest])
 
-  return { setConfirm, loading }
+  return { setSubmit, submitLoading, submitId, setSubmitId }
 }
 
-export default useConfirm
+export default useSubmit
