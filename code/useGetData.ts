4a7c83b75b24f37af0f7691d303e0726548e457f
Glen <104550011+randomtc@users.noreply.github.com>
2023-06-01 14:33:33 +0800

    Update useGetData.ts

--- code/useGetData.ts
+++ code/useGetData.ts
@@ -12,79 +12,80 @@
  * @param  trigger               重新请求的开关
  * @param  loading               请求状态
  */
-import React from "react"
+import React from 'react'
 
 interface ResData<T> {
-  lists: T[]
-  total?: number
-  [k: string]: any
+    lists: T[]
+    total?: number
+    [k: string]: any
 }
 
 interface Res<T> {
-  code: number
-  data: ResData<T>
-  message?: string
-  [k: string]: any
+    code: number
+    data: ResData<T>
+    message?: string
+    [k: string]: any
 }
 
 interface UseGetDataReturn<T> {
-  params: Record<string, any>
-  setParams: React.Dispatch<React.SetStateAction<Record<string, any>>>
-  data?: ResData<T>
-  trigger: boolean
-  setTrigger: React.Dispatch<React.SetStateAction<boolean>>
-  loading: boolean
+    params: Record<string, any>
+    setParams: React.Dispatch<React.SetStateAction<Record<string, any>>>
+    data?: ResData<T> | any
+    trigger: boolean
+    setTrigger: React.Dispatch<React.SetStateAction<boolean>>
+    loading: boolean
 }
 
 const useGetData = <T>(
-  networkRequest: (params: Record<string, any>) => Promise<Res<T>>,
-  varargs?: Record<string, any>,
-  immutableArgs?: { notSend?: boolean; [k: string]: any }
+    networkRequest: any,
+    varargs?: Record<string, any>,
+    immutableArgs?: { notSend?: boolean; [k: string]: any }
 ): UseGetDataReturn<T> => {
-  const [params, setParams] = React.useState<Record<string, any>>({
-    page: 1,
-    page_size: 10,
-    ...varargs,
-  })
-  const [trigger, setTrigger] = React.useState<boolean>(false)
-  const [loading, setLoading] = React.useState<boolean>(false)
-  const [data, setData] = React.useState<ResData<T>>()
+    const [params, setParams] = React.useState<Record<string, any>>({
+        page: 1,
+        page_size: 10,
+        ...varargs
+    })
+    const [trigger, setTrigger] = React.useState<boolean>(false)
+    const [loading, setLoading] = React.useState<boolean>(false)
+    const [data, setData] = React.useState<ResData<T>>()
 
-  const sendRequest = React.useCallback(async () => {
-    setLoading(true)
-    try {
-      const res: Res<T> = await networkRequest({ ...params, ...immutableArgs })
-      if (res?.code === 200) {
-        setData(res?.data)
-      } else {
-        console.error(res)
-      }
-    } catch (err) {
-      console.error(err)
-    } finally {
-      setLoading(false)
-    }
-  }, [networkRequest, params, immutableArgs])
+    const sendRequest = React.useCallback(async () => {
+        setLoading(true)
+        try {
+            const res: Res<T> = await networkRequest({ ...params, ...immutableArgs })
+            if (res?.code === 200) {
+                setData(res?.data)
+            } else {
+                console.error(res)
+            }
+        } catch (err) {
+            console.error(err)
+        } finally {
+            setLoading(false)
+        }
+    }, [networkRequest, params, immutableArgs])
 
-  React.useEffect(() => {
-    if (!immutableArgs?.notSend) {
-      sendRequest()
-    }
-  }, [params, trigger, immutableArgs, sendRequest])
+    React.useEffect(() => {
+        if (!immutableArgs?.notSend) {
+            sendRequest()
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [params, trigger])
 
-  const returnObject: UseGetDataReturn<T> = React.useMemo(
-    () => ({
-      params,
-      setParams,
-      data,
-      trigger,
-      setTrigger,
-      loading,
-    }),
-    [params, data, trigger, loading, setParams, setTrigger]
-  )
+    const returnObject: UseGetDataReturn<T> = React.useMemo(
+        () => ({
+            params,
+            setParams,
+            data,
+            trigger,
+            setTrigger,
+            loading
+        }),
+        [params, data, trigger, loading, setParams, setTrigger]
+    )
 
-  return returnObject
+    return returnObject
 }
 
 export default useGetData
