e15d8cd7946509e597fcecfb5a6a52b35b024cc3
Geshihao <2714780821@qq.com>
2023-11-09 23:47:51 +0800

    updata

--- code/demo/代码题/数据深拷贝.js
+++ code/demo/代码题/数据深拷贝.js
@@ -1,3 +1,4 @@
+//解决循环引用问题，我们可以额外开辟一个存储空间map
 function deepCopy(obj, map = new Map()) {
     //维护一个数组，不需要递归遍历的数据类型（可以自定义拓展）
     const noDeep = [
@@ -11,15 +12,19 @@ function deepCopy(obj, map = new Map()) {
 
     const newData = Array.isArray(obj) ? [] : {}
 
+    // 检查map中有无克隆过的对象有就直接返回
     if (map.get(obj)) return map.get(obj)
 
+    // 没有将当前对象作为key，克隆对象作为value进行存储继续克隆
     map.set(obj, newData)
 
 
     for (let key in obj) {
 
-        if (obj.hasOwnProperty(key)) {
-            newData[key] = deepCopy(obj[key])
+        //对象本身可能自定义hasOwnProperty
+        // if (obj.hasOwnProperty(key)) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            newData[key] = deepCopy(obj[key], map)
         }
     }
 
