28b2e6cc6ce74c4b4d4269983958c9c3f9bd20bc
gsh <2714780821@qq.com>
2025-03-18 17:32:06 +0800

    update

--- code/hooks/useCountDown.ts
+++ code/hooks/useCountDown.ts
@@ -1,53 +1,28 @@
 /**
  * @param  num     倒计时开始时间
  */
-// import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-// interface IndexProps {
-//     num: number
-// }
-
-// const Index = ({ num }: IndexProps) => {
-//     const [time, setTime] = useState(num)
-//     const timeRef = useRef<NodeJS.Timeout>()
-
-//     useEffect(() => {
-//         timeRef.current = setInterval(() => {
-//             setTime(prevTime => prevTime - 1)
-//         }, 1000)
-//         return () => clearInterval(timeRef.current)
-//     }, [])
-
-//     return time
-// }
-
-// export default Index
-import React form 'react'
-
-interface Props {
-    name: string
-
+interface IndexProps {
+    num: number
 }
 
-const useSetCol = (props: Props) => {
+const Index = ({ num }: IndexProps) => {
+    const [time, setTime] = useState(num)
+    const timeRef = useRef<NodeJS.Timeout>()
 
-   useImperativeHandle(
-     first,
-     () => {
-       second
-     },
-     [third],
-   )
+    useEffect(() => {
+        timeRef.current = setInterval(() => {
+            setTime(prevTime => prevTime - 1)
+        }, 1000)
+        return () => clearInterval(timeRef.current)
+    }, [])
 
-   const fn = (forWordRef(ref=>{
-    
-   }))
-
-
-  
-    return size
-    
+    return time
 }
+
+export default Index
+
 
 
 
